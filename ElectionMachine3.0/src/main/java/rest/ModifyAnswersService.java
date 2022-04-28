7f77fd5a269d7af10fa5cc1836f565ab9046372d
silja <90753050+siljaam@users.noreply.github.com>
2022-04-28 14:15:26 +0300

    fixing errors

---
+++ ElectionMachine3.0/src/main/java/rest/ModifyAnswersService.java
@@ -0,0 +1,91 @@
+package rest;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import rest.Answers;
+
+
+@Path("/modifyanswersservice")
+public class ModifyAnswersService {
+	EntityManagerFactory emf=Persistence.createEntityManagerFactory("electionmachine3.0"); 
+	@GET
+	@Path("/readanswers")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public List<Answers> readAnswers() {
+		EntityManager em=emf.createEntityManager();
+		em.getTransaction().begin();
+		List<Answers> list=em.createQuery("select xyx from Fish xyx").getResultList();		//CHANGE LATER
+		em.getTransaction().commit();
+		return list;
+	}	
+	@POST
+	@Path("/addanswers")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public List<Answers> addFish(Answers answers) {
+		EntityManager em=emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(answers);//The actual insertion line
+		em.getTransaction().commit();
+		//Calling the method readFish() of this service
+		List<Answers> list=readAnswers();		
+		return list;
+	}	
+	@PUT
+	@Path("/updateanswers")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public List<Answers> updateFish(Answers answers) {
+		EntityManager em=emf.createEntityManager();
+		em.getTransaction().begin();
+		Answers a =em.find(Answers.class, answers.getKysymys_id());
+	//	Answers a =em.find(Answers.class, answers.getEhdokas_id()); DOES THIS NEED 2 IF SENTENCES ????????
+		if (a!=null) {
+			em.merge(answers);//The actual update line
+		}
+		em.getTransaction().commit();
+		//Calling the method readAnswers() of this service
+		List<Answers> list=readAnswers();		
+		return list;
+	}	
+	@DELETE
+	@Path("/deleteanswers/{id}")									//WE HAVE 2 ID'S ?????
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public List<Answers> deleteAnswers(@PathParam("id") int id) { //WE HAVE 2 ID'S ?????
+		EntityManager em=emf.createEntityManager();
+		em.getTransaction().begin();
+		Answers a=em.find(Answers.class, id);  					//WE HAVE 2 ID'S ????? 
+		if (a!=null) {
+			em.remove(a);//The actual insertion line
+		}
+		em.getTransaction().commit();
+		//Calling the method readAnswers() of this service
+		List<Answers> list=readAnswers();		
+		return list;
+	}	
+	@GET
+	@Path("/readtoupdateanswers/{id}")  				//WE HAVE 2 ID'S ?????
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Answers readToUpdateFish(@PathParam("id") int id) {
+		EntityManager em=emf.createEntityManager();
+		em.getTransaction().begin();
+		Answers a =em.find(Answers.class, id);
+		em.getTransaction().commit();
+		return a;
+	}	
+}
